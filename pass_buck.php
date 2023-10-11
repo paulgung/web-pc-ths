20e2a98d3aa2a8fda60eb525899a684aecf3ca1d
Paul Gung <gongshaoxu@gmail.com>
2023-10-11 19:56:16 +0800

    feat: init

---
+++ pass_buck.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<title>同花顺理财系统法律声明</title>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<link href="http://www.10jqka.com.cn/ia/images/font.css" rel="stylesheet" type="text/css">
+
+
+<link href="http://www.10jqka.com.cn/s/images/pd/css/main_pd.css" rel="stylesheet" type="text/css" />
+<link href="http://s.thsi.cn/css/home/style_v2.min.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+</style>
+<SCRIPT language="JavaScript1.2" src="script/tipmessage.js?abcd=1234" type="text/javascript"></SCRIPT>
+
+</head>
+<body bgcolor="#ffffff" >
+<style type="text/css">
+
+body{padding-top:0px;}
+
+.w950 {width: 950px;
+		text-align: left;
+		margin: 0 auto;
+		clear: both;
+		height: 1%;
+		}
+</style>
+<div class="w950">
+<div class="header clearfix">
+<h3><a href="http://www.10jqka.com.cn/" target="_blank" class="logo"><img alt="同花顺首页" src="http://i.thsi.cn/images/home/logo.gif" class="lazyimg" /></a></h3>
+<ul class="channel">
+    <li><strong><a target="_blank" href="http://news.10jqka.com.cn/">财经</a></strong> 		            <a target="_blank" href="http://news.10jqka.com.cn/fortune_list/">评论</a> 		            <a target="_blank" href="http://news.10jqka.com.cn/cjkx_list/">产经</a> 		            <a target="_blank" href="http://focus.10jqka.com.cn/jrht/">话题</a> 		            <a target="_blank" href="http://pic.10jqka.com.cn/">高清图</a></li>
+    <li><strong><a target="_blank" href="http://stock.10jqka.com.cn/">股票</a></strong> 		            <a target="_blank" href="http://stock.10jqka.com.cn/hsdp_list/">大盘</a> 		            <a target="_blank" href="http://stock.10jqka.com.cn/companynews_list/">公司</a> 		            <a target="_blank" href="http://data.10jqka.com.cn/market/ggsd/ggtype/1/">公告</a> 		            <a target="_blank" href="http://lcjz.10jqka.com.cn/money/money.html">小财神</a></li>
+    <li><strong><a target="_blank" href="http://master.10jqka.com.cn/">名家</a></strong>                     <a target="_blank" href="http://stock.10jqka.com.cn/zljgdx_list/">主力</a>                     <a target="_blank" href="http://stock.10jqka.com.cn/tzjh_list/">机会</a>                     <a target="_blank" href="http://stock.10jqka.com.cn/jiepan_list/">解盘</a>                     <a target="_blank" href="http://stock.10jqka.com.cn/chuangye/">创业板</a></li>
+</ul>
+<ul class="channel">
+    <li><strong><a target="_blank" href="http://fund.10jqka.com.cn">基金</a></strong> 		            <a target="_blank" href="http://fund.10jqka.com.cn/datacenter/jz/">净值</a> 		            <a target="_blank" href="http://fund.10jqka.com.cn/zixuan/">自选</a> 		            <a target="_blank" href="http://fund.10jqka.com.cn/market/">超市</a> 		            <a target="_blank" href="http://fund.10jqka.com.cn/syb/">收益宝</a></li>
+    <li><strong><a target="_blank" href="http://stockpage.10jqka.com.cn/">个股</a></strong> 		            <a target="_blank" href="http://vis.10jqka.com.cn/free/ybzx/index/stockCode/1A0001">研报</a> 		            <a target="_blank" href="http://vis.10jqka.com.cn/free/jgpj/index/">评级</a> 		            <a target="_blank" href="http://search.10jqka.com.cn/thsft/iFindService/StockCalendar">提示</a> 		            <a target="_blank" href="http://data.10jqka.com.cn/market/longhud.html">龙虎榜</a></li>
+    <li><strong><a target="_blank" href="http://yuanchuang.10jqka.com.cn/">原创</a></strong>                     <a target="_blank" href="http://yuanchuang.10jqka.com.cn/djpingpan_list/">评盘</a>                     <a target="_blank" href="http://yuanchuang.10jqka.com.cn/qingbao/">情报</a>                     <a target="_blank" href="http://yuanchuang.10jqka.com.cn/djsjdp_list/">解读</a>                     <a target="_blank" href="http://yuanchuang.10jqka.com.cn/dujiabd_list/">报&nbsp;&nbsp;道</a></li>
+</ul>
+<ul class="channel">
+    <li><strong><a target="_blank" href="http://data.10jqka.com.cn/">数据</a></strong> 		            <a target="_blank" href="http://data.10jqka.com.cn/finance/">财报</a> 		            <a target="_blank" href="http://data.10jqka.com.cn/funds/">资金</a> 		            <a target="_blank" href="http://q.10jqka.com.cn">行情</a> 		            <a target="_blank" href="http://stock.10jqka.com.cn/rzrq/">融资</a></li>
+    <li><strong><a target="_blank" href="http://t.10jqka.com.cn/">圈子</a></strong> 		            <a target="_blank" href="http://blog.10jqka.com.cn/">博客</a> 		            <a target="_blank" href="http://t.10jqka.com.cn/code/1A0001/">股吧</a> 		            <a target="_blank" href="http://ask.10jqka.com.cn/">问股</a> 		            <a target="_blank" href="http://doctor.10jqka.com.cn/">诊股</a></li>
+    <li><strong><a target="_blank" href="http://stock.10jqka.com.cn/newstock/">新股</a></strong>                     <a target="_blank" href="http://stock.10jqka.com.cn/xinsanban/">三板</a>                     <a target="_blank" href="http://stock.10jqka.com.cn/bs/bgzx_list/">B 股</a>                     <a target="_blank" href="http://data.10jqka.com.cn/market/xsjj/">解禁</a>                     <a target="_blank" href="http://stock.10jqka.com.cn/fincalendar.shtml">日历</a></li>
+</ul>
+<ul class="channel">
+    <li><strong><a target="_blank" href="http://goodsfu.10jqka.com.cn/">期货</a></strong> 		            <a target="_blank" href="http://invest.10jqka.com.cn/hj_list/">黄金</a> 		            <a target="_blank" href="http://futures.10jqka.com.cn/">期指</a> 		            <a target="_blank" href="http://bond.10jqka.com.cn/zqzx_list/">债券</a></li>
+    <li><strong><a style="" target="_blank" href="http://q.10jqka.com.cn/qqsc/">全球</a> 		            </strong><a target="_blank" href="http://stock.10jqka.com.cn/hks/">港股</a> 		            <a target="_blank" href="http://stock.10jqka.com.cn/usstock/">美股</a> 		            <a target="_blank" href="http://fe.10jqka.com.cn/">外汇</a></li>
+    <li><strong><a style="" target="_blank" href="http://invest.10jqka.com.cn/">理财</a>                     </strong><a target="_blank" href="http://trust.10jqka.com.cn/">信托</a>                     <a target="_blank" href="http://news.10jqka.com.cn/fc_list/">房产</a>                     <a target="_blank" href="http://invest.10jqka.com.cn/sc_list/">收藏</a></li>
+</ul>
+</div>
+<div class="nav">
+	<a class="hd" target="_blank" href="http://download.10jqka.com.cn/">软件下载</a>
+	<ul class="bd">
+		<li><a target="_blank" href="http://activity.10jqka.com.cn/html/free/140303/" title="同花顺免费版">同花顺免费版</a></li>
+		<li><a style="color: rgb(177, 30, 35);" target="_blank" href="http://www.ths123.com/djr/" title="大金融">大金融</a></li>
+		<li><a target="_blank" href="http://www.ths123.com/dastrategy/index.html" title="大战略">大战略</a></li>
+		<li><a target="_blank" href="http://www.ths123.com/cfxf/index.html" title="财富先锋">财富先锋</a></li>
+		<li><a target="_blank" href="http://www.ths123.com/dyj/" title="大研究">大研究</a></li>
+		<li><a target="_blank" href="http://www.ths123.com/ycs/index.html" title="云计算">云计算</a></li>
+		<li><a style="color: rgb(177, 30, 35);" target="_blank" href="http://sd.10jqka.com.cn/" title="LEVEL-2">level-2</a></li>
+        <li><a target="_blank" href="http://sp.ths123.com/shop/dxb/index/" title="短线宝">短线宝</a></li>
+		<li><a target="_blank" href="http://www.10jqka.com.cn/hkhome/" title="实时港股">实时港股</a></li>
+		<li><a target="_blank" href="http://www.10jqka.com.cn/Futures/" title="股指期货">股指期货</a></li>
+		<li><a target="_blank" href="http://www.10jqka.com.cn/smarttrade/sy.html" title="智能交易">智能交易</a></li>
+		<li><a target="_blank" href="http://www.51ifind.com/" title="iFinD">iFinD</a></li>
+		<li><a target="_blank" href="http://mobile.10jqka.com.cn/" title="手机炒股">手机炒股</a>
+            <a target="_blank" href="http://fund.10jqka.com.cn/public/mobile/?frm=10jqka" title="基金">基金</a>
+        </li>
+	</ul>
+</div>
+</div>
+<div class="w950 mtb5>
+<table width="950" border="0" align="center" cellpadding="0" cellspacing="0" style="margin-top:5px;">
+  <tr>
+    <td bgcolor="#F7F7F7" style="padding:5px;"> 
+      <table border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr> 
+          <td bgcolor="#F7F7F7"> <table width="100%" height="99" border="0" cellpadding="0" cellspacing="0">
+              <tr> 
+                <td width="30" rowspan="3" background="images/xiexian.gif">&nbsp;</td>
+                <td height="30" background="images/xiexian.gif"><div align="center" class="fonttitle">同花顺法律声明</div></td>
+                <td width="30" rowspan="3" background="images/xiexian.gif">&nbsp;</td>
+              </tr>
+              <tr> 
+				  <td class="kuang3"><p>　　<span class="bodyfont">欢迎您使用同花顺金融服务！同花顺金融服务（以下简称同花顺）包括同花顺网站服务,同花顺软件产品,同花顺投资理财服务。请仔细阅读本法律声明。您在同花顺的一切行为将被认为您接受以下服务协议：</span></p>
+                  <p class="bodytitle"> 1)同花顺服务条款的确认</p>
+                  <p>　　<span class="bodyfont">同花顺的各项服务的所有权和运作权归杭州核新软件有限公司。同花顺提供的服务将完全按照其发布的章程、服务条款和操作规则严格执行。用户必须完全同意所有服务条款并完成注册程序，才能成为同花顺的正式用户。</span></p>
+                  <p class="bodytitle">2)服务简介</p>
+                  <p>　　<span class="bodyfont">同花顺运用自己的系统通过国际互联网络为用户提供服务。同时，用户必须：<br />
+                    　　(1)自行配备上网所需设备，包括个人电脑、调制解调器或其他必备上网装置。<br />
+                    　　(2)自行负担个人上网所支付的与此服务有关的电话费用、网络费用。<br />
+                    　　基于同花顺所提供的服务的重要性，用户应同意：<br />
+                    　　(1)提供详尽、准确的个人资料。<br />
+                    　　(2)不断更新注册资料，符合及时、详尽、准确的要求。<br />
+					　　(3)一旦成为同花顺的注册用户，同花顺将通过电子邮件不定期地向用户提供财经、股市及新产品方面的信息和服务，用户将同意和接受，并不视上述行为为侵权行为或为垃圾邮件；<br />
+					　　(4)使用同花顺理财产品，需要准确的计算用户帐户数据，因此要将用户的数据上传到理财服务器进行计算，如果用户选择了数据上传，同花顺视为用户已经同意和接受这种服务。这种服务与用户的交易无关，不包括用户的交易名和密码。仅上传用户的同花顺用户名，和必要的理财数据。<br /><br />
+                    　　同花顺不公开用户的姓名、地址、电子邮箱和笔名，但以下情况例外：<br />
+                    　　(1)用户授权同花顺透露这些信息。<br />
+                    　　(2)相应的法律及程序要求同花顺提供用户的个人资料。如果用户提供的资料包含有不正确的信息，同花顺保留结束用户使用服务资格的权利。　　用户在确认身份开通权限后，即可以获得同花顺提供的相应产品服务．该产品服务一经确认，不可更换其它产品．</span></p>
+                  <p> <span class="bodytitle">3)服务条款的修改和服务修订</span>　</p>
+                  <p>　　<span class="bodyfont">同花顺有权在必要时修改服务条款，同花顺服务条款一旦发生变动，将会在重要页面上提示修改内容。如果不同意所改动的内容，用户可以主动取消获得的服务。如果用户继续享用服务，则视为接受服务条款的变动。同花顺保留随时修改或中断免费服务而不需知照用户的权利。同花顺行使修改或中断免费服务的权利，不需对用户或第三方负责。</span></p>
+                  <p class="bodytitle">4)用户隐私制度</p>
+                  <p>　　<span class="bodyfont">尊重用户个人隐私是同花顺的一项基本政策。所以，作为对以上第二点个人注册资料分析的补充，同花顺一定不会在未经合法用户授权时公开、编辑或透露其注册资料及其它保存在同花顺中的非公开内容，除非有法律许可要求或同花顺在诚信的基础上认为透露这些信件在以下四种情况是必要的：<br />
+                    　　(1)遵守有关法律规定，遵从同花顺合法服务程序。<br />
+                    　　(2)维护同花顺的商标所有权。<br />
+                    　　(3)在紧急情况下竭力维护用户个人和社会大众的隐私安全。<br />
+                    　　(4)符合其他相关的要求。</span></p>
+                  <p class="bodytitle">5)用户的帐号，密码和安全性</p>
+                  <p>　　<span class="bodyfont">用户一旦注册成功，成为同花顺的合法用户，将得到一个密码和用户名。用户将对用户名和密码安全负全部责任。另外，每个用户都要对以其用户名进行的所有活动和事件负全责。您可随时根据指示改变您的密码。用户若发现任何非法使用用户帐号或存在安全漏洞的情况，请立即通告同花顺。同花顺将尽可能保证您的注册信息、安全传输并且妥善保存。同花顺仅在为您提供服务时使用以上信息。</span></p>
+                  <p class="bodytitle">6)风险免责</p>
+                  <p> 　　<span class="bodyfont">用户个人对服务的使用承担风险。同花顺对此不作任何类型的担保。同花顺不担保服务一定能满足用户的要求，也不担保服务不会受中断，对服务的及时性，安全性，出错发生都不作担保。同花顺对在同花顺上得到的任何信息服务或交易进程不作担保。同花顺提供的包括同花顺理财的所有文章，数据，不构成任何的投资建议，用户查看或依据这些内容所进行的任何行为造成的风险和结果都自行负责，与同花顺无关。</span></p>
+                  <p class="bodytitle">7)有限责任</p>
+                  <p>　　<span class="bodyfont">同花顺对任何直接、间接、偶然、特殊及继起的损害不负责任，这些损害可能来自：不正当使用网络服务，在网上购买商品或进行同类型服务，在网上进行交易，非法使用网络服务或用户传送的信息有所变动。这些行为都有可能会导致同花顺的形象受损，所以同花顺事先提出这种损害的可能性。此外，为了网站的正常运行，同花顺需要定期或不定期地对网站进行停机维护，因此类情况而造成的正常服务中断，请您予以理解。同花顺将尽力避免服务中断或将中断时间限制在最短时间内。如因不可抗力或其他无法控制的原因，使同花顺系统崩溃或无法正常使用，导致您在同花顺丢失有关的信息、记录等，同花顺不承担责任。但是同花顺会尽合理的可能协助处理善后事宜，并努力使您免受经济损失。</span></p>
+                  <p><span class="bodytitle">8)对用户信息的存储和限制</span>　　</p>
+                  <p class="bodyfont">　　同花顺不对用户所发布信息的删除或储存失败负责。同花顺有判定用户行为是否符合同花顺服务条款要求的权利，如果用户违背了服务条款的规定，同花顺有中断对其提供网络服务的权利。</p>
+                  <p><span class="bodytitle">9)用户管理</span>　　</p>
+                  <p class="bodyfont">　　用户单独承担发布内容的责任。用户对服务的使用是根据所有适用于同花顺的国家法律、地方法律和国际法律标准的。用户必须遵循：</p>
+                  <p>　　<span class="bodyfont">(1)从中国境内向外传输技术性资料时必须符合中国有关法规。<br />
+                    　　(2)使用网络服务不作非法用途。<br />
+                    　　(3)不干扰或混乱网络服务。<br />
+                    　　(4)遵守所有使用网络服务的网络协议、规定、程序和惯例。</span></p>
+                  <p> 　　<span class="bodyfont">用户须承诺不传输任何非法的、骚扰性的、中伤他人的、辱骂性的、恐吓性的、伤害性的、庸俗的，淫秽等信息资料。另外，用户也不能传输任何教唆他人构成犯罪行为的资料；不能传输助长国内不利条件和涉及国家安全的资料；不能传输任何不符合当地法规、国家法律和国际法律的资料。未经许可而非法进入其它电脑系统是禁止的。若用户的行为不符合以上提到的服务条款，同花顺将作出独立判断立即取消用户服务帐号。用户需对自己在网上行为承担法律责任。用户若在同花顺上散布和传播反动、色情或其他违反国家法律的信息，同花顺的系统记录有可能作为用户违反法律的证据。</span></p>
+                  <p class="bodytitle">10)保障</p>
+                  <p>　　<span class="bodyfont">用户同意保障和维护同花顺全体成员的利益，负责支付由用户使用超出服务范围引起的律师费用，违反服务条款的损害补偿费用等。</span></p>
+                  <p class="bodytitle"> 11)停止服务</p>
+                  <p>　　<span class="bodyfont">用户或同花顺可随时根据实际情况中断一项或多项网络服务。同花顺不需对任何个人或第三方负责而随时中断服务。用户对后来的条款修改有异议，或对同花顺的服务不满，可以行使如下权利：<br />
+                    　　(1)停止使用同花顺的网络服务。<br />
+                    　　(2)通告同花顺停止对该用户的服务。<br />
+                    　　结束用户服务后，用户使用网络服务的权利马上中止。从那时起，用户没有权利，同花顺也没有义务传送任何未处理的信息或未完成的服务给用户或第三方。</span></p>
+                  <p class="bodytitle">12)通告</p>
+                  <p>　　<span class="bodyfont">所有发给用户的通告都可通过重要页面的公告或电子邮件或常规的信件传送。服务条款的修改、服务变更、或其它重要事件的通告都会以此形式进行。</span></p>
+                  <p class="bodytitle">13)参与广告策划</p>
+                  <p>　　<span class="bodyfont">用户在他们发表的信息中加入宣传资料或参与广告策划，在同花顺的免费服务上展示他们的产品，任何这类促销方法，包括运输货物、付款、服务、商业条件、担保及与广告有关的描述都只是在相应的用户和广告销售商之间发生。同花顺不承担任何责任，同花顺没有义务为这类广告销售负任何一部分的责任。</span></p>
+                  <p class="bodytitle">14)服务内容的所有权</p>
+                  <p>　　<span class="bodyfont">同花顺定义的网络服务内容包括：文字、软件、声音、图片、录象、图表、广告中的全部内容；电子邮件的全部内容；同花顺为用户提供的其他信息。所有这些内容受版权、商标、标签和其它财产所有权法律的保护。所以，用户只能在同花顺和广告商授权下才能使用这些内容，而不能擅自复制、再造这些内容、或创造与内容有关的派生产品。同花顺所有的文章版权归原文作者和同花顺共同所有，任何人需要转载同花顺的文章，必须征得原文作者或同花顺授权。</span></p>
+                  <p class="bodytitle">15)法律</p>
+                  <p>　　<span class="bodyfont">本网络服务条款要与中华人民共和国的法律解释相一致，用户和同花顺一致同意服从高等法院所有管辖。如发生同花顺服务条款与中华人民共和国法律相抵触时，则这些条款将完全按法律规定重新解释，而其它条款则依旧保持对用户产生法律效力和影响。</span></p>
+                  <p class="bodytitle">16)免责条款</p>
+                  <p>　　<span class="bodyfont">同花顺对因证券交易所、卫星传输线路、电信部门及网络服务提供商等任何原因造成的信息传递异常情况所至后果不承担法律责任，但有义务积极协助交易所、电信部门、网络服务提供商使之恢复正常。用户应始终避免并消除由于上述原因造成的信息遗漏、错误、丢失、延迟、中断等可能给同花顺带来不利影响的因素，使其免受经济损失和名誉损失，不得因本合同关系而以上述缘由向同花顺提出索赔。此外，由于地震、台风、战争、罢工、政府行为、瘟疫、爆发性和流行性传染病或其他重大疫情、非因各方原因造成的火灾、基础电信网络中断造成的及其它各方不能预见并且对其发生后果不能预防或避免的不可抗力原因，致使相关服务中断，同花顺不承担由此产生的损失，但应及时通知服务中断原因，并积极加以解决。</span></p></td>
+              </tr>
+              <tr> 
+                <td height="30" background="images/xiexian.gif">&nbsp;</td>
+              </tr>
+            </table></td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+</div></div>
+  <div id="footer" style="font-family:tahoma">
+  <p>
+  <a href="http://news.10jqka.com.cn/tzz/" rel="nofollow">投资者关系</a><span class="ff">|</span>
+  <a href="http://news.10jqka.com.cn/20100105/c61833421.shtml" rel="nofollow">关于同花顺</a><span>|</span>
+  <a href="http://download.10jqka.com.cn/">股票软件</a><span>|</span>
+  <a href="http://www.10jqka.com.cn/ia/pass_buck.php" rel="nofollow">法律声明</a><span>|</span>
+  <a href="http://www.10jqka.com.cn/hexin_license.htm" rel="nofollow">运营许可</a><span>|</span>
+  <a href="http://www.10jqka.com.cn/modules.php?name=what&amp;page=cooperate" rel="nofollow">内容合作</a><span>|</span>
+  <a href="http://www.10jqka.com.cn/hexin_contact.htm" rel="nofollow">联系我们</a><span>|</span>
+  <a href="http://news.10jqka.com.cn/link.shtml" rel="nofollow">友情链接</a><span>|</span>
+  <a href="http://news.10jqka.com.cn/msg/" rel="nofollow">网友意见箱</a><span>|</span>
+  <a href="http://job.10jqka.com.cn/" rel="nofollow">招聘英才</a>
+  </p>
+  <p class="c333">Copyright&copy;Zhejiang Hithink Flush Information Network Co., Ltd. All rights reserved.</p>
+  <p class="c333"> 浙江核新同花顺网络信息股份有限公司版权所有</p>	
+  <div style="display:none">
+  <script type="text/javascript">
+  var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+  document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F78c58f01938e4d85eaf619eae71b4ed1' type='text/javascript'%3E%3C/script%3E"));
+  </script><script src=" http://hm.baidu.com/h.js?78c58f01938e4d85eaf619eae71b4ed1" type="text/javascript"></script>
+  </div>
+  </div>
+  </div></body></html>
